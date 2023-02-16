7c22ebfb127157bb8bed3564780252dceb9a754e
kevinc2465 <chenk5104@gmail.com>
2023-02-16 13:13:52 -0500

    something

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,26 +1,26 @@
 import "../styles/style.css";
 import { DOM } from "./DOM";
 
-const URL = "https://valorant-api.com/v1/agents";
+const URL = "https://valorant-api.com/v1/agents/";
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
-    const datas = await response.json();
-    datas.forEach((data) => {
-      console.log(data.displayName);
-    });
+    const infos = await response.json();
+    console.log(infos.data[0].displayName);
     // infos.forEach((info) => {
-    //   // DOM.agents.insertAdjacentHTML(
-    //   //   "beforeend",
-    //   //   `
-    //   //     <div class="result">
-    //   //     <h2 class="beverage-name">${info.displayName}</h2>
-    //   //     </div>
-    //   //   `
-    //   // );
-    //   console.log(info.displayName);
+    //   DOM.agents.insertAdjacentHTML(
+    //     "beforeend",
+    //     `
+    //       <div class="result">
+    //       <h2>${info.data.displayName}</h2>
+    //       </div>
+    //     `
+    //   );
     // });
+    infos.forEach((info) => {
+      console.log(info.data.uuid);
+    });
   } catch (error) {
     console.log(error);
   }
